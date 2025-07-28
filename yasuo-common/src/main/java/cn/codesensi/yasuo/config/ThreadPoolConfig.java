a5a3f2a24e2c4a22bd1c29673d6a6746cb85e2c2
codesensi <codesensi@aliyun.com>
2025-07-28 21:16:49 +0800

    perf: 线程池配置异常捕获优化

--- yasuo-common/src/main/java/cn/codesensi/yasuo/config/ThreadPoolConfig.java
+++ yasuo-common/src/main/java/cn/codesensi/yasuo/config/ThreadPoolConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -41,8 +40,7 @@ public class ThreadPoolConfig {
             // 反射加载拒绝策略类
             Class<?> clazz = Class.forName("java.util.concurrent.ThreadPoolExecutor$" + threadPoolProperties.getRejectedExecutionHandler());
             executor.setRejectedExecutionHandler((RejectedExecutionHandler) clazz.getDeclaredConstructor().newInstance());
-        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException |
-                 IllegalAccessException e) {
+        } catch (Exception e) {
             // 默认使用CallerRunsPolicy策略：直接在execute方法的调用线程中运行被拒绝的任务
             executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         }
